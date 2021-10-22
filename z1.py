02e2f3d1f37c1929ca3327f473c2307a28e4b11d
Ilya Mescheryakov <mescheryakov.i.a.99@mail.ru>
2021-10-22 21:30:04 +0300

    add problem

--- z1.py
+++ z1.py
@@ -6,10 +6,14 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.rate_lecturer = []
     def __set__(self):
-        res = (f'Имя: = {self.name} \n Фамилия: = {self.surname} \n Средняя оценка за домашние задания: = {self.grades} \n'
-               f'Курсы в процессе изучения: = {self.courses_in_progress} \n Завершенные курсы: = {self.finished_courses} \n')
+        res = (
+            f'Имя: = {self.name} \n Фамилия: = {self.surname} \n Средняя оценка за домашние задания: = {self.grades} \n'
+            f'Курсы в процессе изучения: = {self.courses_in_progress} \n Завершенные курсы: = {self.finished_courses} \n')
         return res
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -17,23 +21,18 @@ class Mentor:
         self.courses_attached = []
 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-
-
-
-
 class Lecturer(Mentor):
-    def __init__(self, read_lectures, get_rate):
-        self.read_lectures = read_lectures
+    def __init__(self, name, surname, read_lectures, get_rate):
+        super().__init__(name, surname)
         self.read_lectures = []
         self.get_rate = []
+
+
     def __set__(self):
         res = (f'Имя: = {self.name} \n Фамилия: = {self.surname} \n Средняя оценка за лекции: = {self.get_rate}')
         return res
+
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -49,8 +48,16 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
     def __set__(self):
         res = (f'Имя: = {self.name} \n Фамилия: = {self.surname} \n ')
         return res
 
 
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+Student.rate_lecture = [10, 8, 7, 9, 10]
